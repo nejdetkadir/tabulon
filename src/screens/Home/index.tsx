6db419cdb1e70a2d7fec43214edb701a645b91e8
Nejdet Kadir Bektaş <50639655+nejdetkadir@users.noreply.github.com>
2023-07-19 19:38:05 +0300

    feat: add i18n support (#3)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+
+import { useLocale } from '@app/hooks';
+
 const Home: React.FC = () => {
+  const SCOPE_OPTIONS = {
+    scope: 'screens.Home',
+  };
+
+  const navigation = useNavigation();
+  const { t } = useLocale();
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: t('labels.title', SCOPE_OPTIONS),
+    });
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>{t('labels.title', SCOPE_OPTIONS)}</Text>
     </View>
   );
 };
