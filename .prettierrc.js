8e373ee4b3ac1233069b7012d5fc5848472ae648
nejdetkadir <nejdetkadir.550@gmail.com>
2023-07-19 18:48:51 +0300

    feat: add react navigation

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
+  semi: true,
   trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 140,
+  tabWidth: 2,
+  useTabs: false,
 };
